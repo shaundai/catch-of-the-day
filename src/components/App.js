e218ee7aa58e0216e61ce33a2b85b5d4e481410d
Shaundai Person <shaundaiperson@gmail.com>
2019-12-07 00:03:10 -0500

    add more firebase functionality

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,26 @@ class App extends React.Component {
         order: {}
     };
 componentDidMount(){
-    const { params } = this.props.match
-    this.ref = base.syncState(`${params.storeId}/fishes`)
+    const { params } = this.props.match;
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if(localStorageRef){
+        this.setState({
+            order: JSON.parse(localStorageRef)
+        })
+    }
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+        context: this,
+        state: 'fishes'
+    })
 }
 
+componentDidUpdate(){
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+}
+
+componentWillUnmount(){
+    base.removeBinding(this.ref);
+}
 
     addFish = fish => {
         const fishes = { ...this.state.fishes }; // use spread operator to make a clone of existing state instead of mutating array
